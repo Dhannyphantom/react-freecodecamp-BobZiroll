0db68916041ccde420303e388f71bcf3ae2fbbbd
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-06-04 13:57:31 +0100

    Organizing Components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,7 @@
 import "./App.css";
-
-function MainContent() {
-  return (
-    <>
-      <h1>Excited About React!</h1>
-
-      <ol>
-        <li>Pricing</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ol>
-    </>
-  );
-}
-
-function Header() {
-  return (
-    <header>
-      <nav className="nav">
-        <img src="./logo512.png" className="nav-logo" alt="" />
-        <ul className="listHeader">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
-
-function Footer() {
-  return (
-    <footer className="footer">
-      <small>"@REACT page 2022"</small>
-    </footer>
-  );
-}
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 function App() {
   return (
---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+export default function MainContent() {
+  return (
+    <>
+      <h1>Excited About React!</h1>
+
+      <ol>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ol>
+    </>
+  );
+}
