3181a5c781f7c55fc8747abcee90f17041be008d
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-06-04 15:08:16 +0100

    NavBar for project one done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,11 @@
+import reactLogo from "../assets/logo192.png";
+
 export default function NavBar() {
-  return <h1>NavBar goes here! </h1>;
+  return (
+    <nav className="nav">
+      <img src={reactLogo} alt="" className="nav-logo" />
+      <h1>ReactFacts</h1>
+      <p>React Course - Project 1</p>
+    </nav>
+  );
 }
