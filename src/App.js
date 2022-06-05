a80f07965c69df06bbcff74a7fad234e08934b48
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-06-05 21:03:15 +0100

    SOLO PROJECT INITIALISED

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import "./App.css";
-import Card from "./components/Card";
-import Hero from "./components/Hero";
-import NavBar from "./components/NavBar";
-import dataSet from "./api/dataSet";
-
-const { cardDetail } = dataSet;
 
 function App() {
-  const cardElements = cardDetail.map((obj) => {
-    return <Card key={obj.id} item={obj} />;
-  });
-
-  return (
-    <div className="container">
-      <NavBar />
-      <Hero />
-      <section className="card-list">{cardElements}</section>
-    </div>
-  );
+  return <div className="container"></div>;
 }
 
 export default App;
--- src/components/NavBar.js
+++
@@ -1,9 +0,0 @@
-import logo from "../assets/airbnb-logo.png";
-
-export default function NavBar() {
-  return (
-    <nav className="nav">
-      <img src={logo} className="nav-logo" alt="" />
-    </nav>
-  );
-}
