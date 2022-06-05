11bede3e708ebeaffc124576ec6d0b701f40ff5a
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-06-05 21:30:37 +0100

    Detail Header done for Solo project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import DetailTitle from "./components/DetailTitle";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="container">
       <NavBar />
+      <DetailTitle />
     </div>
   );
 }
