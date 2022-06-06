f1a59b65e55d7e087e592992cf21f17a2b9ea845
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-06-06 01:09:05 +0100

    NavBar improved

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,10 @@ import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export default function NavBar() {
   return (
-    <nav className="nav">
+    <header className="nav">
       <FontAwesomeIcon icon={solid("smile-wink")} size="2x" color="white" />
       <h2>Meme Generator</h2>
       <p>React Course - Project 3</p>
-    </nav>
+    </header>
   );
 }
